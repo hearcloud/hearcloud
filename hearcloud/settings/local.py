9c08e683cb3806341865260fad8eff0a255460db
mpvillafranca <mpvillafranca@correo.ugr.es>
2016-08-23 17:17:52 +0200

    removed STACTIC_FILE_DIRS

--- hearcloud/settings/local.py
+++ hearcloud/settings/local.py
@@ -22,7 +22,6 @@ if LOCAL_DEVELOPMENT:
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- hearcloud/settings/staging.py
+++ hearcloud/settings/staging.py
@@ -40,7 +40,6 @@ if STAGING_ENVIRONMENT:
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
